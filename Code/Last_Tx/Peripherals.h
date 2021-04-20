55ae93c978d36a6b515285c6fdb36f0c5dcb697c
elchavodel11 <ignacio11ch@gmail.com>
2021-04-19 19:27:08 -0700

    Tx Test Controller
    Attempts to obtain values from pots and joystick, prints them and tries to send to receiver (Rx)

---
+++ Code/Last_Tx/Peripherals.h
@@ -0,0 +1,13 @@
+#ifndef Peripherals_h //Checks if main has already defined header file
+#define Peripherals_h
+#include <Arduino.h>
+
+struct Peripherals
+{
+  byte jX;            // joystick X val
+  byte jY;            // joystick Y val
+  byte pot1;          // Pot 1 (blue)
+  byte pot2;          // Pot 2 (red)
+
+};
+#endif
